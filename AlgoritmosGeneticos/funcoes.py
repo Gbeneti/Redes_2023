5c3355784d48d59bd013fef5b44c924b42b9a54d
Gustavo Alves Beneti <gustavo220061@ilum.cnpem.br>
2023-03-16 20:15:45 -0300

    minor fixes alg. geneticos exp. 3

--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -4,7 +4,7 @@ def gene_cb():
     '''Gera um gene válido para o problema
     
     Return:
-    Um valor zero ou um.
+        Um valor zero ou um.
     '''
     
     
@@ -16,10 +16,10 @@ def individuo_cb(n):
     '''Gera um indivíduo das caixas binárias.
     
     Args:
-    n: número de genes do indivíduo
+        n: número de genes do indivíduo
     
     Return:
-    Uma lista de genes, de modo que os valores aceitos são 0 e 1.
+        Uma lista de genes, de modo que os valores aceitos são 0 e 1.
     '''
     individuo = []
     for i in range(0,n,1): #repete as funções n vezes
@@ -30,10 +30,10 @@ def individuo_cb(n):
 def funcao_objetivo_cb(individuo):
     """Computa a função objetivo no problema das caixas binárias.        
     Args:      
-    individuo: lista contendo os genes das caixas binárias.        
+        individuo: lista contendo os genes das caixas binárias.        
     
     Return:      
-    Um valor representando a soma dos genes do indivíduo.    
+        Um valor representando a soma dos genes do indivíduo.    
     """
     return sum(individuo)
 
@@ -41,11 +41,11 @@ def populacao_cb(tamanho,genes):
     '''Cria uma população no problema das caixas binárias.
     
     Returns:
-    Uma lista onde cada item é um indivíduo. Um indivíduo é uma lista (ou um conjunto de listas) com "n" genes.
+        Uma lista onde cada item é um indivíduo. Um indivíduo é uma lista (ou um conjunto de listas) com "n" genes.
     
     Args:
-    tamanho: tamanho da população
-    genes: quantidade de genes
+        tamanho: tamanho da população
+        genes: quantidade de genes
     '''
     populacao = []
     for _ in range(0,tamanho,1): #usar "_" apenas para a situação de não precisar do valor do "for"
@@ -60,11 +60,11 @@ def selecao_roleta_max(populacao, fitness):
     Nota_2: retorna uma população de mesmo tamanho do input.
     
     Return: 
-    População dos indivíduos selecionados
+        População dos indivíduos selecionados
     
     Args:
-    populacao: lista com todos os individuos da populacao
-    fitness: lista com o valor da funcao objetivo dos individuos da populcao
+        populacao: lista com todos os individuos da populacao
+        fitness: lista com o valor da funcao objetivo dos individuos da populcao
     
     '''
     
@@ -74,9 +74,13 @@ def selecao_roleta_max(populacao, fitness):
     
     
 def funcao_objetivo_pop_cb(populacao):
-    """Calcula a funcao objetivo para todos os membros de uma população        
-        Args:      populacao: lista com todos os indivíduos da população       
-        Return:      Lista de valores representando a fitness de cada indivíduo de uma população.    
+    """Calcula a funcao objetivo para todos os membros de uma população
+    
+        Args:     
+            populacao: lista com todos os indivíduos da população       
+        
+        Return:      
+            Lista de valores representando a fitness de cada indivíduo de uma população.    
         
         """    
     
@@ -90,16 +94,19 @@ def cruzamento_ponto_simples(pai,mae):
     '''Operador de cruzamento de ponto simples.
     
     Args:
-    pai: uma lista representando um indivíduo
-    mãe: uma lista representando um indivíduo
+        pai: uma lista representando um indivíduo
+        mãe: uma lista representando um indivíduo
     
     Return:
-    Duas listas, sendo que cada uma representa um filho dos pais que foram os argumentos
+        Duas listas, sendo que cada uma representa um filho dos pais que foram os argumentos
     '''
     
     ponto_de_corte = random.randint(1, ((len(pai))-1)) #na lista
     filho1 = pai[:ponto_de_corte] + mae[ponto_de_corte:]
-    filho2 = mae[:ponto_de_corte] + mae[ponto_de_corte:]
+    filho2 = mae[:ponto_de_corte] + pai[ponto_de_corte:]
+    
+#filho1: pega todos os genes do pai até 'ponto_de_corte', concatena com todos os genes da mãe, menos "ponto_de_corte"
+#filho2: pega todos os genes do mãe até 'ponto_de_corte', concatena com todos os genes da pai, menos "ponto_de_corte"
     
     return filho1,filho2
 
@@ -107,10 +114,10 @@ def mutacao_cb(individuo):
     '''Realiza a mutação de um gene no problema das caixas binárias
     
     Args:
-    individuo: uma lista representado um individuo no papel das caixas vinárias
+        individuo: uma lista representado um individuo no papel das caixas vinárias
     
     Return:
-    Um indivíduo com um gene mutado
+        Um indivíduo com um gene mutado
     
     '''
     
